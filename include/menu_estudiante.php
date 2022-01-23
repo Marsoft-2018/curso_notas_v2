780bb0f1ce273c630fe9b99f449b6353ab9f4cba
Jose Tapia <josealf7@gmail.com>
2022-01-22 19:05:13 -0500

    ajuste menu estudiante

--- include/menu_estudiante.php
+++ include/menu_estudiante.php
@@ -1,15 +1,11 @@
 <ul class="sidebar-menu" data-widget="tree">
         <li class="header">Menu Estudiantes</li>
-       
- <li class="treeview">
-          
-
-         <a onclick="cargar_contenido('contenido_principal','reportes/consultar_notas.php')">
-             <i class="fa  fa-cog"></i> <span>Consultas Notas </span>
-             <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-             </span>
-            </a>
-       </li>
-     </ul>
-         
+        <li class="treeview">
+                <a onclick="cargar_contenido('contenido_principal','reportes/consultar_notas.php')">
+                        <i class="fa  fa-cog"></i> <span>Consultas Notas </span>
+                        <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                </a>
+        </li>
+</ul>
